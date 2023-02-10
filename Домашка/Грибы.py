f88c737d3f8c3e00e55595776403550883733f0d
tam-artem <tam-artem@mail.ru>
2023-02-10 17:56:14 +0300

    Домашнее задание №1

--- Домашка/Грибы.py
+++ Домашка/Грибы.py
@@ -1,8 +1,12 @@
 num = int(input())
-if num // 100:
+if num % 100 >= 11 and num % 100 <= 14:
     print("Грибов")
-elif num // 10 % 10:
+elif num % 10 == 1:
+    print("Гриб")
+elif num % 10 >= 2 and num % 10 <= 4:
     print("Гриба")
-elif num % 10:
+else:
     print("Грибов")
+
+
 
